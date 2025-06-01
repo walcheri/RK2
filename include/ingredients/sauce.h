99657df4840a8dfa72650eda19cd4c5b521ad196
walcheri <vagzvrf@mail.ru>
2025-06-01 21:01:08 +0300

    RK2

--- include/ingredients/sauce.h
+++ include/ingredients/sauce.h
@@ -2,13 +2,13 @@
 #include "hamburger_ingredient.h"
 
 struct Sauce : public HamburgerIngredient {
-    std::string Name() const override { return "Sauce" };
+    std::string Name() const override { return "Sauce"; };
 };
 
 struct GarlicSauce : public Sauce {
-    std::string Name() const override { return "GarlicSauce" };
+    std::string Name() const override { return "GarlicSauce"; };
 };
 
 struct MustardSauce : public Sauce {
-    std::string Name() const override { return "MustardSauce" };
+    std::string Name() const override { return "MustardSauce"; };
 };
