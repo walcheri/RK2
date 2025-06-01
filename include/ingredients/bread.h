bbe3fa068d802c6eaa8e5e203dd8b562acd714e1
walcheri <vagzvrf@mail.ru>
2025-06-01 20:49:42 +0300

    RK2

--- include/ingredients/bread.h
+++ include/ingredients/bread.h
@@ -2,13 +2,13 @@
 #include "hamburger_ingredient.h"
 
 struct Bread : public HamburgerIngredient {
-    std::string Name() const override;
+    std::string Name() const override { return "Bread"; }
 };
 
 struct LightBread : public Bread {
-    std::string Name() const override;
+    std::string Name() const override { return "LightBread"; }
 };
 
 struct SaltyBread : public Bread {
-    std::string Name() const override;
+    std::string Name() const override { return "SaltyBread"; }
 };
