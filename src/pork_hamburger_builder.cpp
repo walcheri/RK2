559bea393423dc41232b9d36c6f65c48770ccbbc
walcheri <vagzvrf@mail.ru>
2025-06-01 20:42:17 +0300

    RK2

--- src/pork_hamburger_builder.cpp
+++ src/pork_hamburger_builder.cpp
@@ -1,10 +1,10 @@
 #include "../include/pork_hamburger_builder.h"
-#include "../include/ingredients/salty_bread.h"
-#include "../include/ingredients/pork_patty.h"
-#include "../include/ingredients/cheddar_cheese.h"
+#include "../include/ingredients/bread.h"
+#include "../include/ingredients/patty.h"
+#include "../include/ingredients/cheese.h"
 #include "../include/ingredients/tomato.h"
 #include "../include/ingredients/lettuce.h"
-#include "../include/ingredients/mustard_sauce.h"
+#include "../include/ingredients/sauce.h"
 
 void PorkHamburgerBuilder::Initialize() { ingredientList_.clear(); }
 void PorkHamburgerBuilder::BuildBread() { ingredientList_.push_back(std::make_unique<SaltyBread>()); }
