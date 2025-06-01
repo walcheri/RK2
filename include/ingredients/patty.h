569a09d54aaa800787cea1309acb44551ead4ef4
walcheri <vagzvrf@mail.ru>
2025-06-01 20:50:05 +0300

    RK2

--- include/ingredients/patty.h
+++ include/ingredients/patty.h
@@ -2,13 +2,13 @@
 #include "hamburger_ingredient.h"
 
 struct Patty : public HamburgerIngredient {
-    std::string Name() const override;
+    std::string Name() const override { return "Patty"; }
 };
 
 struct BeefPatty : public Patty {
-    std::string Name() const override;
+    std::string Name() const override { return "BeefPatty"; }
 };
 
 struct PorkPatty : public Patty {
-    std::string Name() const override;
+    std::string Name() const override { return "PorkPatty"; }
 };
