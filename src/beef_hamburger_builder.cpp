aa6516468d251fa71c1c8d09a53e1be41809e005
walcheri <vagzvrf@mail.ru>
2025-06-01 20:41:24 +0300

    RK2

--- src/beef_hamburger_builder.cpp
+++ src/beef_hamburger_builder.cpp
@@ -1,10 +1,10 @@
 #include "../include/beef_hamburger_builder.h"
-#include "../include/ingredients/light_bread.h"
-#include "../include/ingredients/beef_patty.h"
-#include "../include/ingredients/mozzarella_cheese.h"
+#include "../include/ingredients/bread.h"
+#include "../include/ingredients/patty.h"
+#include "../include/ingredients/cheese.h"
 #include "../include/ingredients/tomato.h"
 #include "../include/ingredients/lettuce.h"
-#include "../include/ingredients/garlic_sauce.h"
+#include "../include/ingredients/sauce.h"
 
 void BeefHamburgerBuilder::Initialize() { ingredientList_.clear(); }
 void BeefHamburgerBuilder::BuildBread() { ingredientList_.push_back(std::make_unique<LightBread>()); }
