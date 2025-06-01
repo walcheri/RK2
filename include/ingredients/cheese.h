c33de10f44385110365a699cc638c26182c53ae0
walcheri <vagzvrf@mail.ru>
2025-06-01 20:57:35 +0300

    RK2

--- include/ingredients/cheese.h
+++ include/ingredients/cheese.h
@@ -2,13 +2,13 @@
 #include "hamburger_ingredient.h"
 
 struct Cheese : public HamburgerIngredient {
-    std::string Name() const override;
+    std::string Name() const override { return "Cheese" };
 };
 
 struct MozzarellaCheese : public Cheese {
-    std::string Name() const override;
+    std::string Name() const override { return "MozzarellaCheese" };
 };
 
 struct CheddarCheese : public Cheese {
-    std::string Name() const override;
+    std::string Name() const override { return "CheddarCheese" };
 };
